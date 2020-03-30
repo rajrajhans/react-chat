3c34757c104845f56d45b65d8e7ccec9c7557b43
Raj Rajhans <raj.rajhans18@vit.edu>
2020-03-31 04:19:05 +0530

    completed chatList component

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,12 +1,57 @@
 import React, {Component} from 'react';
+import ChatListComponent from '../chatlist/chatList'
+const firebase = require("firebase");
 
 class DashboardComponent extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            selectedChat : null,
+            newChatFormVisible : false,
+            email : null,
+            chats : []
+        }
+    }
+
     render() {
         return (
-            <div>
-
-            </div>
+            <ChatListComponent history={this.props.history}
+                               newChatBtnFn={this.newChatBtnClicked}
+                               selectChatFn={this.selectChat}
+                               chats={this.state.chats}
+                               userEmail={this.state.email}
+                               selectedChatIndex={this.state.selectedChat}
+            />
         );
+    }
+    selectChat = (chatIndex) =>{
+        console.log("Selected a chat", chatIndex);
+    };
+
+    newChatBtnClicked = () => {
+        this.setState({newChatFormVisible:true, selectedChat:false})
+    };
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(async _user => {
+            if(!_user)
+                this.props.history.push('/login');
+            else{
+                await firebase
+                    .firestore()
+                    .collection('chats')
+                    .where('users', 'array-contains', _user.email)
+                    .onSnapshot(async res => {
+                        const chats = res.docs.map(_doc => _doc.data())
+                        await this.setState({
+                            email: _user.email,
+                            chats : chats
+                        });
+                        console.log(this.state)
+                    })
+            }
+        })
     }
 }
 
